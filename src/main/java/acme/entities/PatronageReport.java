2088fd0411cd41744b7dc27c1cd163552611f482
Juan Manuel Talero Frias <juatalfri@alum.us.es>
2022-03-14 18:24:24 +0100

    Creacion de patronageReport

---
+++ src/main/java/acme/entities/PatronageReport.java
@@ -0,0 +1,61 @@
+package acme.entities;
+
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity {
+
+	// Serialisation identifier
+	
+	protected static final long serialVersionUID = 1L;
+	
+	//Attributes
+	
+	@Past
+	@NotNull
+	protected LocalDate creationDate;
+	
+	@NotBlank
+	@Length(max = 255)
+	@NotNull
+	protected String memorandum;
+	
+	@URL
+	protected String moreInfo;
+		
+	//Derived attributes
+	
+	@NotBlank
+	protected String serialCode() {
+			
+		String serialNumber = Integer.toString(this.id);
+		
+		while (serialNumber.length() < 4) {
+			serialNumber = "0" + serialNumber;
+		}
+		
+		return this.patronage.getCode() + ":" + serialNumber;
+	}
+	
+	//Relationships
+	
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Patronage patronage;
+}
