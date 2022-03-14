11cc858df51c73deba8ceacd9eb10d23c6c59617
DanielE <danenrdia@alum.us.es>
2022-03-14 19:17:55 +0100

    Creada entidad PatronDashboard

---
+++ src/main/java/acme/forms/PatronDashboard.java
@@ -0,0 +1,35 @@
+package acme.forms;
+
+import java.io.Serializable;
+
+public class PatronDashboard implements Serializable {
+
+	private static final long	serialVersionUID	= 1L;
+		
+	Integer totalNumberPatronagesOfStatus;
+	Double averageBudgetPatronagesOfStatusByCurrency;
+	Double deviationBudgetPatronagesOfStatusByCurrency;
+	Double minimumBudgetPatronagesOfStatusByCurrency;
+	Double maximumBudgetPatronagesOfStatusByCurrency;
+	
+//	Integer						totalNumberProposedPatronages;
+//	Integer						totalNumberAcceptedPatronages;
+//	Integer						totalNumberDeniedPatronages;
+//
+//	Double						averageBudgetProposedPatronagesByCurrency;
+//	Double						averageBudgetAcceptedPatronagesByCurrency;
+//	Double						averageBudgetDeniedPatronagesByCurrency;
+//
+//	Double						deviationBudgetProposedPatronagesByCurrency;
+//	Double						deviationBudgetAcceptedPatronagesByCurrency;
+//	Double						deviationBudgetDeniedPatronagesByCurrency;
+//
+//	Double						minimumBudgetProposedPatronagesByCurrency;
+//	Double						minimumBudgetAcceptedPatronagesByCurrency;
+//	Double						minimumBudgetDeniedPatronagesByCurrency;
+//
+//	Double						maximumBudgetProposedPatronagesByCurrency;
+//	Double						maximumBudgetAcceptedPatronagesByCurrency;
+//	Double						maximumBudgetDeniedPatronagesByCurrency;
+
+}
