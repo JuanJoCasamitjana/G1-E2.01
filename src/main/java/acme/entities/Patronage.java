d9c33764781119932fc5904bc340a7a7d037f746
Juan Manuel Talero Frias <juatalfri@alum.us.es>
2022-03-15 17:52:56 +0100

    Se ha añadido varios atributos temporales y modificado restricciones

--- src/main/java/acme/entities/Patronage.java
+++ src/main/java/acme/entities/Patronage.java
@@ -1,20 +1,24 @@
 package acme.entities;
 
-import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
-import org.hibernate.validator.constraints.time.DurationMin;
 
+import acme.framework.datatypes.Money;
 import acme.framework.entities.AbstractEntity;
 import acme.roles.Inventor;
 import acme.roles.Patron;
@@ -32,31 +36,45 @@ public class Patronage extends AbstractEntity {
 	
 	//Attributes
 	
-	@NotNull
 	protected Status status;
 	
-	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$", message = "wrong pattern, example -> ABZ-019-A")
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$", message = "wrong pattern, example -> ABZ-019(-A)optional")
 	@Column(unique = true)
-	@NotNull
+	@NotBlank
 	protected String code;
 	
 	@NotBlank
 	@Length(max = 255)
-	@NotNull
 	protected String legalStuff;
 	
-	@Positive
+	@Min(1)
+	@Valid
 	@NotNull
-	protected Double budget;
+	protected Money budget;
 	
-	@DurationMin(days = 30)
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	protected Duration duration;
+	protected LocalDate creationDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected LocalDate startDate;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected LocalDate finishDate;
 	
 	@URL
 	protected String moreInfo;
 	
 	//Derived attributes
+	
+	@NotNull
+	protected Period period() {
+					
+		return Period.between(this.finishDate, this.startDate);
+	}
 	
 	//Relationships
 	
