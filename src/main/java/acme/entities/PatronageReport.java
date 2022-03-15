2b618d835fa7c97fe868555a280c708ce4f2e19d
Juan Manuel Talero Frias <juatalfri@alum.us.es>
2022-03-15 17:58:13 +0100

    Se han modificado algunas restricciones

--- src/main/java/acme/entities/PatronageReport.java
+++ src/main/java/acme/entities/PatronageReport.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -28,12 +31,12 @@ public class PatronageReport extends AbstractEntity {
 	//Attributes
 	
 	@Past
+	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
 	protected LocalDate creationDate;
 	
 	@NotBlank
 	@Length(max = 255)
-	@NotNull
 	protected String memorandum;
 	
 	@URL
@@ -56,6 +59,7 @@ public class PatronageReport extends AbstractEntity {
 	//Relationships
 	
 	@NotNull
+	@Valid
 	@ManyToOne(optional = false)
 	protected Patronage patronage;
 }
