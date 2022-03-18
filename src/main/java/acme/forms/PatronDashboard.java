66903a1788f0b58d8c90e078835be19a2837c130
DanielE <danenrdia@alum.us.es>
2022-03-18 19:59:25 +0100

    Cambios de atributos en PatronDashboard
    Se ha cambiado los Integer y Double en Maps para aumentar la facilidad
    en los siguientes deliverables

--- src/main/java/acme/forms/PatronDashboard.java
+++ src/main/java/acme/forms/PatronDashboard.java
@@ -2,7 +2,11 @@
 package acme.forms;
 
 import java.io.Serializable;
+import java.util.Map;
 
+import org.springframework.data.util.Pair;
+
+import acme.entities.Status;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,15 +16,16 @@ public class PatronDashboard implements Serializable {
 
 	// Serialisation identifier -----------------------------------------------
 
-	private static final long serialVersionUID = 1L;
+	private static final long			serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
-	Integer						totalNumberPatronagesOfStatus;
-	Double						averageBudgetPatronagesOfStatusByCurrency;
-	Double						deviationBudgetPatronagesOfStatusByCurrency;
-	Double						minimumBudgetPatronagesOfStatusByCurrency;
-	Double						maximumBudgetPatronagesOfStatusByCurrency;
+	Map<Status, Integer>				totalNumberPatronagesOfStatus;
+	
+	Map<Pair<String, Status>, Double>	averageBudgetPatronagesOfStatusByCurrency;
+	Map<Pair<String, Status>, Double>	deviationBudgetPatronagesOfStatusByCurrency;
+	Map<Pair<String, Status>, Double>	minimumBudgetPatronagesOfStatusByCurrency;
+	Map<Pair<String, Status>, Double>	maximumBudgetPatronagesOfStatusByCurrency;
 
 	// Derived attributes -----------------------------------------------------
 
