c061d2455e240ec20c88771985cce5dc77fb1581
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-03-31 17:54:08 +0200

    Actualizacion del proyecto versiones 22.2 y 22.3

--- src/main/java/acme/features/authenticated/consumer/AuthenticatedConsumerController.java
+++ src/main/java/acme/features/authenticated/consumer/AuthenticatedConsumerController.java
@@ -16,14 +16,12 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import acme.framework.controllers.AbstractController;
 import acme.framework.roles.Authenticated;
 import acme.roles.Consumer;
 
 @Controller
-@RequestMapping("/authenticated/consumer/")
 public class AuthenticatedConsumerController extends AbstractController<Authenticated, Consumer> {
 
 	// Internal state ---------------------------------------------------------
