0a1a917f1b482f4ad834888967309ef4e18d47f3
Andres Vargas <andvarbay@alum.us.es>
2022-04-19 20:33:07 +0200

    Task-24: Done
    Task-24: operations by all principals on components done.

---
+++ src/main/java/acme/features/any/item/AnyItemController.java
@@ -0,0 +1,30 @@
+package acme.features.any.item;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.Item;
+import acme.framework.controllers.AbstractController;
+import acme.framework.roles.Any;
+
+@Controller
+public class AnyItemController extends AbstractController<Any, Item>{
+
+	// Internal state ---------------------------------------------------------
+	
+	@Autowired
+	protected AnyItemListComponentService listComponentService;
+	
+	@Autowired
+	protected AnyItemShowService showService;
+	
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list-component", "list", this.listComponentService);
+		super.addCommand("show", this.showService);
+	}
+		
+}
