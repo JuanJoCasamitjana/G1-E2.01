701f53a9c41301df7d98b1e84a006c51f8be2fb9
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-04-24 11:33:32 +0200

    Avance test inventor tool-kit

--- src/test/java/acme/testing/inventors/toolkits/InventorToolKitListShowTest.java
+++ src/test/java/acme/testing/inventors/toolkits/InventorToolKitListShowTest.java
@@ -11,14 +11,25 @@ public class InventorToolKitListShowTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/tool-kit/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex,final String code, final String name, 
-		final String title, final String description, final String assemblyNotes, 
-		final String optionalLink) {
+	public void positiveTest(final int recordIndex,final String code, final String title, 
+		final String description, final String assemblyNotes, final String optionalLink) {
 		super.signIn("inventor1", "inventor1");
-		super.clickOnMenu("Inventor", "");
+		super.clickOnMenu("Inventors", "My toolkits");
 		super.checkListingExists();
 		super.sortListing(1, "asc");
 
+		super.checkListingExists();
+		super.checkColumnHasValue(recordIndex, 0 ,code );
+		super.checkColumnHasValue(recordIndex, 1,title );
+		super.clickOnListingRecord(0);
+		
+		super.checkFormExists();
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("assemblyNotes", assemblyNotes);
+		super.checkInputBoxHasValue("optionalLink", optionalLink);
+		
 		super.signOut();
 	}
 }
