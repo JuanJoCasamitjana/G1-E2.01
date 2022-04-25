362a66e6ea5e859299079b5a9de2e04eed13e9f8
vicgrabru <vgravanbru@gmail.com>
2022-04-25 21:47:19 +0200

    Arreglando bugs

--- src/main/java/acme/entities/ToolKit.java
+++ src/main/java/acme/entities/ToolKit.java
@@ -50,6 +50,8 @@ public class ToolKit extends AbstractEntity{
 	protected String optionalLink;
 	
 	@NotNull
+	protected boolean published;
+	
 	@Valid
 	@ManyToOne(optional = false)
 	protected Inventor inventor;
