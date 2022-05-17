93c5f8ba1a420cf6f2c910cc8e5ec45b7662527e
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-05-17 17:27:48 +0200

    Preparacion Task-60

---
+++ src/main/java/acme/features/administrator/systemConfiguration/AdministratorSystemConfigrationPerformService.java
@@ -0,0 +1,68 @@
+package acme.features.administrator.systemConfiguration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Errors;
+import acme.framework.controllers.Request;
+import acme.framework.roles.Administrator;
+import acme.framework.services.AbstractUpdateService;
+import acme.system.configuration.SystemConfiguration;
+
+@Service
+public class AdministratorSystemConfigrationPerformService implements AbstractUpdateService<Administrator, SystemConfiguration> {
+	@Autowired
+	protected AdministratorSystemConfigrationRepository repository;
+
+	@Override
+	public boolean authorise(final Request<SystemConfiguration> request) {
+		assert request != null;
+		
+		return true;
+	}
+
+	@Override
+	public void bind(final Request<SystemConfiguration> request, final SystemConfiguration entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		request.bind(entity, errors, "acceptedCurrencies","defaultSystemCurrency", "strongSpamTerms", "strongSpamThreshold", "weakSpamTerms", "weakSpamThreshold");
+		
+	}
+
+	@Override
+	public void unbind(final Request<SystemConfiguration> request, final SystemConfiguration entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		
+		request.unbind(entity, model, "acceptedCurrencies","defaultSystemCurrency", "strongSpamTerms", "strongSpamThreshold", "weakSpamTerms", "weakSpamThreshold");
+		model.setAttribute("readOnly", false);
+	}
+
+	@Override
+	public SystemConfiguration findOne(final Request<SystemConfiguration> request) {
+		SystemConfiguration sysConf;
+		sysConf = this.repository.getSystemConfiguration();
+		
+		return sysConf;
+	}
+
+	@Override
+	public void validate(final Request<SystemConfiguration> request, final SystemConfiguration entity, final Errors errors) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(final Request<SystemConfiguration> request, final SystemConfiguration entity) {
+		assert request != null;
+		assert entity != null;
+		
+		this.repository.save(entity);
+		
+	}
+
+}
--- src/main/webapp/WEB-INF/views/administrator/system-configuration/form.jsp
+++ src/main/webapp/WEB-INF/views/administrator/system-configuration/form.jsp
@@ -3,7 +3,7 @@
 <%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix="acme" uri="urn:jsptagdir:/WEB-INF/tags"%>
 
-<acme:form readonly="true">
+<acme:form readonly="${readOnly}">
 	<acme:input-textbox code="administrator.system-configuration.form.label.default-system-currency" path="defaultSystemCurrency"/>
 	<acme:input-textbox code="administrator.system-configuration.form.label.accepted-currencies" path="acceptedCurrencies"/>
 	<acme:input-textbox code="administrator.system-configuration.form.label.strong-spam-terms" path="strongSpamTerms"/>
